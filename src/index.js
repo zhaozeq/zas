dd6cb740eb1a4714c9c79002e89fbda8e14265c7
赵泽清 <wb-zzq461950@alibaba-inc.com>
2019-02-27 15:08:04 +0800

    first c

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import log, { isString, isNode } from "log-tips"
-const app = {}
+import log, { isString, isNode } from 'log-tips';
 
-app.start = start
+const app = {};
+
+app.start = start;
 
 function start(container) {
   // 允许 container 是字符串，然后用 querySelector 找元素
   if (isString(container)) {
-    container = document.querySelector(container)
-    log(container, `container ${container} not found`)
+    container = document.querySelector(container);
+    log(container, `container ${container} not found`);
   }
 
   // 并且是 HTMLElement
-  invariant(
-    !container || isNode(container),
-    `[app.start] container should be HTMLElement`
-  )
+  invariant(!container || isNode(container), '[app.start] container should be HTMLElement');
 
   // 路由必须提前注册
   // invariant(
@@ -22,18 +20,18 @@ function start(container) {
   //   `[app.start] router must be registered before app.start()`
   // )
   if (!app._store) {
-    oldAppStart.call(app)
-    console.log(app._store, "app._store")
+    oldAppStart.call(app);
+    console.log(app._store, 'app._store');
   }
-  const store = app._store
+  const store = app._store;
 
-  app._getProvider = getProvider.bind(null, store, app)
+  app._getProvider = getProvider.bind(null, store, app);
 
   // If has container, render; else, return react component
   if (container) {
-    render(container, store, app, app._router)
-    app._plugin.apply("onHmr")(render.bind(null, container, store, app))
+    render(container, store, app, app._router);
+    app._plugin.apply('onHmr')(render.bind(null, container, store, app));
   } else {
-    return getProvider(store, this, this._router)
+    return getProvider(store, this, this._router);
   }
 }
